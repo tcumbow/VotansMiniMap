ee617eedae4c83608a542aab00301e56addd3436
Tom Cumbow <tcumbow@icloud.com>
2021-05-22 23:10:31 -0400

    Deploy.ps1

---
+++ Deploy.ps1
@@ -0,0 +1,4 @@
+robocopy .\VotansMiniMap A:\VotansMiniMap /mir
+robocopy .\VotansMiniMap S:\VotansMiniMap /mir
+robocopy .\VotansMiniMap J:\VotansMiniMap /mir
+robocopy .\VotansMiniMap V:\AddonsGoldCopy\VotansMiniMap /mir
